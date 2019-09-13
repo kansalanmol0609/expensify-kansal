5c689b710ad16c42289af7bb9aea136fcfa4203d
Anmol Kansal <41195162+kansalanmol0609@users.noreply.github.com>
2019-09-14 01:40:44 +0530

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -4,19 +4,12 @@ import ReactDOM from 'react-dom';
 //We now need not to pass store to each and every component
 //we will use Provider instead that will make store available to each component
 import { Provider } from 'react-redux';
-import { addExpense } from './actions/expenses';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configureStore();
-
-store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: -1000 }));
-store.dispatch(addExpense({ description: 'Water Bill', amount: 1000, createdAt: -2000 }));
-store.dispatch(addExpense({ description: 'Coffee', amount: 300, createdAt: 1000 }));
-
-console.log(store.getState());
 
 const jsx = (
     //Only prop we need to pass is store that you want to share with all of the components
