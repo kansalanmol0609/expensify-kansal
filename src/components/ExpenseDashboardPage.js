31b5f15f0e41f09212a851d9a578dd9b37833d5e
Anmol Kansal <41195162+kansalanmol0609@users.noreply.github.com>
2019-09-14 12:52:40 +0530

    expenses total heading added

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters';
+import ExpenseSummary from './ExpensesSummary';
 
 const ExpenseDashboardPage = () => (
     <div>
+        <ExpenseSummary />
         <ExpenseListFilters />
         <ExpenseList />
     </div>
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
-import HelpPage from '../components/HelpPage';
 import notFoundPage from '../components/notFoundPage';
 import Header from '../components/Header';
 
@@ -15,7 +14,6 @@ const AppRouter = () => (
                 <Route path="/" component={ExpenseDashboardPage} exact={true} />
                 <Route path="/create" component={AddExpensePage} />
                 <Route path="/edit/:id" component={EditExpensePage} />
-                <Route path="/help" component={HelpPage} />
                 <Route component={notFoundPage} />
             </Switch>
         </div>
