89544be6d539f24a68d891d950d42cd54d8759d0
Anmol Kansal <41195162+kansalanmol0609@users.noreply.github.com>
2019-09-14 12:05:37 +0530

    setup dev dependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = (env) => {
     return {
         entry: './src/app.js',
         output: {
-            path: path.join(__dirname, 'public'),
+            path: path.join(__dirname, 'public', 'dist'),
             filename: 'bundle.js'
         },
         // mode: 'development',
@@ -54,6 +54,7 @@ module.exports = (env) => {
         devServer: {
             contentBase: path.join(__dirname, 'public'),
             historyApiFallback: true,
+            publicPath: '/dist/',
             port: 8080
         }
     };
