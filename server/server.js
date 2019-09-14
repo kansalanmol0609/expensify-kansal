df8b33d70b4c010e3663bd738f3604521e3f5df5
Anmol Kansal <41195162+kansalanmol0609@users.noreply.github.com>
2019-09-14 11:03:23 +0530

    Setup Production

---
+++ server/server.js
@@ -0,0 +1,16 @@
+// require is the node way of importing things
+const express = require('express');
+const app = express();
+const path = require('path');
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log('Server is Up!');
+})
